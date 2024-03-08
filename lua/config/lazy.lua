252681fd2c8a3a14ee53739dce9fc611467f7361
Kasper Helweg Jonassen <khjcph@gmail.com>
2024-03-08 09:40:31 +0100

    update symbols out line and ocaml lang (#1)
    Co-authored-by: Kasper Helweg Jonassen <khe@Kasper-Helweg-Jonassen.local>

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -27,8 +27,9 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.editor.symbols-outline" },
+    { import = "lazyvim.plugins.extras.editor.outline" },
     -- { import = "lazyvim.plugins.extras.editor.aerial" },
+    { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
 
     { import = "plugins.languages" },
