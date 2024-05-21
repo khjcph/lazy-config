ef8ee1e5497315469614e3bff727c024270c8c8b
khjcph <khjcph@gmail.com>
2024-05-21 12:35:28 +0200

    add trouble v3

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -84,8 +84,15 @@ return {
   {
     "folke/trouble.nvim",
 
-    opts = {
-      position = "left",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+      -- { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+      -- { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false win.position=left<cr>",
+        desc = "Symbols (Trouble)",
+      },
     },
   },
 
